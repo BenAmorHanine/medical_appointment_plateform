b1b3cb156f02213df3e732b2e159563bd93562a0
fatmatrabelsi17 <fatma.trabelsi@insat.ucar.tn>
2026-01-29 20:40:03 +0100

    fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,13 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
-import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  
-  // Use cookie parser for secure session management
-  app.use(cookieParser());
   
   // CORS with credentials support
   app.enableCors({
