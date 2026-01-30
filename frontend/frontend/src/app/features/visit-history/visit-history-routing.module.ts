a42b2dfcb206477401f679257debaa527b6bffd0
fatmatrabelsi17 <fatma.trabelsi@insat.ucar.tn>
2026-01-30 14:26:56 +0100

    Style enhanced

--- frontend/frontend/src/app/features/visit-history/visit-history-routing.module.ts
+++ frontend/frontend/src/app/features/visit-history/visit-history-routing.module.ts
@@ -1,37 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { VisitHistoryComponent } from './component/visit-history/visit-history.component';
-import { authGuard } from '../auth/guards/auth.guard';
 import { DoctorPatientsComponent } from './component/doctor-patients/doctor-patients.component';
-
-/*
-const routes: Routes = [
-  // 👤 Patient
-  {
-    path: '',
-    component: VisitHistoryComponent,
-  },
-
-  // 👨‍⚕️ Médecin → liste des patients
-  {
-    path: 'doctor',
-    component: DoctorPatientsComponent,
-  },
-   {
-  path: 'history/doctor/patient',
-  component: VisitHistoryComponent,
-}
-,
-/*
-  // 👨‍⚕️ Médecin → historique d’un patient
-  {
-    path: 'doctor/patient/:patientId',
-    component: VisitHistoryComponent,
-  },
-
- 
-];
-*/
 
 const routes: Routes = [
   {
